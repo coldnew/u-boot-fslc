a462259e9388af1136556d474d40fc15dbc86acb
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-09-07 09:19:30 +0000

    first commit from wandboard_android-4.4.2_fullsource_20150303.tar.xz
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

---
+++ board/wandboard/wandboard_spl.c
@@ -0,0 +1,488 @@
+/*
+ * Author: Tungyi Lin <tungyilin1127@gmail.com>
+ *
+ * Derived from EDM_CF_IMX6 code by TechNexion,Inc
+ *
+ * SPDX-License-Identifier:     GPL-2.0+
+ */
+#include <common.h>
+#include <asm/io.h>
+#include <asm/arch/sys_proto.h>
+#ifdef CONFIG_SPL
+#include <spl.h>
+#endif
+
+#define CONFIG_SPL_STACK	0x0091FFB8
+
+DECLARE_GLOBAL_DATA_PTR;
+
+#if defined(CONFIG_SPL_BUILD)
+
+static enum boot_device boot_dev;
+enum boot_device get_boot_device(void);
+
+static inline void setup_boot_device(void)
+{
+	uint soc_sbmr = readl(SRC_BASE_ADDR + 0x4);
+	uint bt_mem_ctl = (soc_sbmr & 0x000000FF) >> 4 ;
+	uint bt_mem_type = (soc_sbmr & 0x00000008) >> 3;
+	uint bt_mem_mmc = (soc_sbmr & 0x00001000) >> 12;
+
+	switch (bt_mem_ctl) {
+	case 0x0:
+		if (bt_mem_type)
+			boot_dev = MX6_ONE_NAND_BOOT;
+		else
+			boot_dev = MX6_WEIM_NOR_BOOT;
+		break;
+	case 0x2:
+			boot_dev = MX6_SATA_BOOT;
+		break;
+	case 0x3:
+		if (bt_mem_type)
+			boot_dev = MX6_I2C_BOOT;
+		else
+			boot_dev = MX6_SPI_NOR_BOOT;
+		break;
+	case 0x4:
+	case 0x5:
+		if (bt_mem_mmc)
+			boot_dev = MX6_SD0_BOOT;
+		else
+			boot_dev = MX6_SD1_BOOT;
+		break;
+	case 0x6:
+	case 0x7:
+		boot_dev = MX6_MMC_BOOT;
+		break;
+	case 0x8 ... 0xf:
+		boot_dev = MX6_NAND_BOOT;
+		break;
+	default:
+		boot_dev = MX6_UNKNOWN_BOOT;
+		break;
+	}
+}
+
+enum boot_device get_boot_device(void) {
+	return boot_dev;
+}
+
+#include "asm/arch/mx6_ddr_regs.h"
+/*
+ *   0x30 == 40 Ohm
+ *   0x28 == 48 Ohm
+ */
+
+#define DRIVE_STRENGTH_IMX6DL 0x28U
+
+static void spl_dram_init_mx6solo_512mb(void);
+static void spl_dram_init_mx6dl_1g(void);
+static void spl_dram_init_mx6q_2g(void);
+static void spl_dram_init(void);
+
+static void spl_mx6q_dram_setup_iomux(void)
+{
+	volatile struct mx6qd_iomux_ddr_regs *mx6q_ddr_iomux;
+	volatile struct mx6qd_iomux_grp_regs *mx6q_grp_iomux;
+
+	mx6q_ddr_iomux = (struct mx6dq_iomux_ddr_regs *) MX6DQ_IOM_DDR_BASE;
+	mx6q_grp_iomux = (struct mx6dq_iomux_grp_regs *) MX6DQ_IOM_GRP_BASE;
+
+	mx6q_grp_iomux->grp_ddr_type = (u32)0x000c0000;
+	mx6q_grp_iomux->grp_ddrpke = (u32)0x00000000;
+	mx6q_ddr_iomux->dram_sdclk_0 = (u32)0x00000030;
+	mx6q_ddr_iomux->dram_sdclk_1 = (u32)0x00000030;
+	mx6q_ddr_iomux->dram_cas = (u32)0x00000030;
+	mx6q_ddr_iomux->dram_ras = (u32)0x00000030;
+	mx6q_grp_iomux->grp_addds = (u32)0x00000030;
+	mx6q_ddr_iomux->dram_reset = (u32)0x00000030;
+	mx6q_ddr_iomux->dram_sdba2 = (u32)0x00000000;
+	mx6q_ddr_iomux->dram_sdodt0 = (u32)0x00000030;
+	mx6q_ddr_iomux->dram_sdodt1 = (u32)0x00000030;
+	mx6q_grp_iomux->grp_ctlds = (u32)0x00000030;
+	mx6q_grp_iomux->grp_ddrmode_ctl = (u32)0x00020000;
+	mx6q_ddr_iomux->dram_sdqs0 = (u32)0x00000030;
+	mx6q_ddr_iomux->dram_sdqs1 = (u32)0x00000030;
+	mx6q_ddr_iomux->dram_sdqs2 = (u32)0x00000030;
+	mx6q_ddr_iomux->dram_sdqs3 = (u32)0x00000030;
+	mx6q_ddr_iomux->dram_sdqs4 = (u32)0x00000030;
+	mx6q_ddr_iomux->dram_sdqs5 = (u32)0x00000030;
+	mx6q_ddr_iomux->dram_sdqs6 = (u32)0x00000030;
+	mx6q_ddr_iomux->dram_sdqs7 = (u32)0x00000030;
+	mx6q_grp_iomux->grp_ddrmode = (u32)0x00020000;
+	mx6q_grp_iomux->grp_b0ds = (u32)0x00000030;
+	mx6q_grp_iomux->grp_b1ds = (u32)0x00000030;
+	mx6q_grp_iomux->grp_b2ds = (u32)0x00000030;
+	mx6q_grp_iomux->grp_b3ds = (u32)0x00000030;
+	mx6q_grp_iomux->grp_b4ds = (u32)0x00000030;
+	mx6q_grp_iomux->grp_b5ds = (u32)0x00000030;
+	mx6q_grp_iomux->grp_b6ds = (u32)0x00000030;
+	mx6q_grp_iomux->grp_b7ds = (u32)0x00000030;
+	mx6q_ddr_iomux->dram_dqm0 = (u32)0x00000030;
+	mx6q_ddr_iomux->dram_dqm1 = (u32)0x00000030;
+	mx6q_ddr_iomux->dram_dqm2 = (u32)0x00000030;
+	mx6q_ddr_iomux->dram_dqm3 = (u32)0x00000030;
+	mx6q_ddr_iomux->dram_dqm4 = (u32)0x00000030;
+	mx6q_ddr_iomux->dram_dqm5 = (u32)0x00000030;
+	mx6q_ddr_iomux->dram_dqm6 = (u32)0x00000030;
+	mx6q_ddr_iomux->dram_dqm7 = (u32)0x00000030;
+}
+
+static void spl_mx6dl_dram_setup_iomux(void)
+{
+	volatile struct mx6sdl_iomux_ddr_regs *mx6dl_ddr_iomux;
+	volatile struct mx6sdl_iomux_grp_regs *mx6dl_grp_iomux;
+
+	mx6dl_ddr_iomux = (struct mx6sdl_iomux_ddr_regs *) MX6SDL_IOM_DDR_BASE;
+	mx6dl_grp_iomux = (struct mx6sdl_iomux_grp_regs *) MX6SDL_IOM_GRP_BASE;
+
+	mx6dl_grp_iomux->grp_ddr_type = (u32)0x000c0000;
+	mx6dl_grp_iomux->grp_ddrpke = (u32)0x00000000;
+	mx6dl_ddr_iomux->dram_sdclk_0 = (u32)DRIVE_STRENGTH_IMX6DL;
+	mx6dl_ddr_iomux->dram_sdclk_1 = (u32)DRIVE_STRENGTH_IMX6DL;
+	mx6dl_ddr_iomux->dram_cas = (u32)DRIVE_STRENGTH_IMX6DL;
+	mx6dl_ddr_iomux->dram_ras = (u32)DRIVE_STRENGTH_IMX6DL;
+	mx6dl_grp_iomux->grp_addds = (u32)DRIVE_STRENGTH_IMX6DL;
+	mx6dl_ddr_iomux->dram_reset = (u32)DRIVE_STRENGTH_IMX6DL;
+	mx6dl_ddr_iomux->dram_sdcke0 = (u32)DRIVE_STRENGTH_IMX6DL;
+	mx6dl_ddr_iomux->dram_sdcke1 = (u32)DRIVE_STRENGTH_IMX6DL;
+	mx6dl_ddr_iomux->dram_sdba2 = (u32)0x00000000;
+	mx6dl_ddr_iomux->dram_sdodt0 = (u32)DRIVE_STRENGTH_IMX6DL;
+	mx6dl_ddr_iomux->dram_sdodt1 = (u32)DRIVE_STRENGTH_IMX6DL;
+	mx6dl_grp_iomux->grp_ctlds = (u32)DRIVE_STRENGTH_IMX6DL;
+	mx6dl_grp_iomux->grp_ddrmode_ctl = (u32)0x00020000;
+	mx6dl_ddr_iomux->dram_sdqs0 = (u32)DRIVE_STRENGTH_IMX6DL;
+	mx6dl_ddr_iomux->dram_sdqs1 = (u32)DRIVE_STRENGTH_IMX6DL;
+	mx6dl_ddr_iomux->dram_sdqs2 = (u32)DRIVE_STRENGTH_IMX6DL;
+	mx6dl_ddr_iomux->dram_sdqs3 = (u32)DRIVE_STRENGTH_IMX6DL;
+	mx6dl_ddr_iomux->dram_sdqs4 = (u32)DRIVE_STRENGTH_IMX6DL;
+	mx6dl_ddr_iomux->dram_sdqs5 = (u32)DRIVE_STRENGTH_IMX6DL;
+	mx6dl_ddr_iomux->dram_sdqs6 = (u32)DRIVE_STRENGTH_IMX6DL;
+	mx6dl_ddr_iomux->dram_sdqs7 = (u32)DRIVE_STRENGTH_IMX6DL;
+	mx6dl_grp_iomux->grp_ddrmode = (u32)0x00020000;
+	mx6dl_grp_iomux->grp_b0ds = (u32)DRIVE_STRENGTH_IMX6DL;
+	mx6dl_grp_iomux->grp_b1ds = (u32)DRIVE_STRENGTH_IMX6DL;
+	mx6dl_grp_iomux->grp_b2ds = (u32)DRIVE_STRENGTH_IMX6DL;
+	mx6dl_grp_iomux->grp_b3ds = (u32)DRIVE_STRENGTH_IMX6DL;
+	mx6dl_grp_iomux->grp_b4ds = (u32)DRIVE_STRENGTH_IMX6DL;
+	mx6dl_grp_iomux->grp_b5ds = (u32)DRIVE_STRENGTH_IMX6DL;
+	mx6dl_grp_iomux->grp_b6ds = (u32)DRIVE_STRENGTH_IMX6DL;
+	mx6dl_grp_iomux->grp_b7ds = (u32)DRIVE_STRENGTH_IMX6DL;
+	mx6dl_ddr_iomux->dram_dqm0 = (u32)DRIVE_STRENGTH_IMX6DL;
+	mx6dl_ddr_iomux->dram_dqm1 = (u32)DRIVE_STRENGTH_IMX6DL;
+	mx6dl_ddr_iomux->dram_dqm2 = (u32)DRIVE_STRENGTH_IMX6DL;
+	mx6dl_ddr_iomux->dram_dqm3 = (u32)DRIVE_STRENGTH_IMX6DL;
+	mx6dl_ddr_iomux->dram_dqm4 = (u32)DRIVE_STRENGTH_IMX6DL;
+	mx6dl_ddr_iomux->dram_dqm5 = (u32)DRIVE_STRENGTH_IMX6DL;
+	mx6dl_ddr_iomux->dram_dqm6 = (u32)DRIVE_STRENGTH_IMX6DL;
+	mx6dl_ddr_iomux->dram_dqm7 = (u32)DRIVE_STRENGTH_IMX6DL;
+}
+
+static void spl_dram_init_mx6solo_512mb(void)
+{
+	volatile struct mmdc_p_regs *mmdc_p0;
+	volatile struct mmdc_p_regs *mmdc_p1;
+	mmdc_p0 = (struct mmdc_p_regs *) MMDC_P0_BASE_ADDR;
+	mmdc_p1 = (struct mmdc_p_regs *) MMDC_P1_BASE_ADDR;
+
+	/* ZQ */
+	mmdc_p0->mpzqhwctrl = (u32)0xA1390003;
+
+	/* Write leveling */
+	mmdc_p0->mpwldectrl0 = (u32)0x0053005A;
+	mmdc_p0->mpwldectrl1 = (u32)0x00430048;
+
+	mmdc_p0->mpdgctrl0 = (u32)0x024C0244;
+	mmdc_p0->mpdgctrl1 = (u32)0x0234023C;
+	mmdc_p0->mprddlctl = (u32)0x3E484A44;
+	mmdc_p0->mpwrdlctl = (u32)0x3A38302E;
+	/* Read data bit delay */
+	mmdc_p0->mprddqby0dl = (u32)0x33333333;
+	mmdc_p0->mprddqby1dl = (u32)0x33333333;
+	mmdc_p0->mprddqby2dl = (u32)0x33333333;
+	mmdc_p0->mprddqby3dl = (u32)0x33333333;
+
+	/* Complete calibration by forced measurement */
+	mmdc_p0->mpmur0 = (u32)0x00000800;
+	mmdc_p0->mdpdc = (u32)0x0002002D;
+	mmdc_p0->mdotc = (u32)0x00333040;
+	mmdc_p0->mdcfg0 = (u32)0x3F435313;
+	mmdc_p0->mdcfg1 = (u32)0xB66E8B63;
+	mmdc_p0->mdcfg2 = (u32)0x01FF00DB;
+
+	mmdc_p0->mdmisc = (u32)0x00011740;
+	mmdc_p0->mdscr = (u32)0x00008000;
+	mmdc_p0->mdrwd = (u32)0x000026D2;
+	mmdc_p0->mdor = (u32)0x00431023;
+	mmdc_p0->mdasp = (u32)0x00000017;
+	mmdc_p0->mdctl = (u32)0x83190000;
+	mmdc_p0->mdscr = (u32)0x02008032;
+	mmdc_p0->mdscr = (u32)0x00008033;
+	mmdc_p0->mdscr = (u32)0x00048031;
+	mmdc_p0->mdscr = (u32)0x05208030;
+	mmdc_p0->mdscr = (u32)0x04008040;
+
+	mmdc_p0->mdref = (u32)0x00007800;
+	mmdc_p0->mpodtctrl = (u32)0x00022227;
+	mmdc_p0->mdpdc = (u32)0x0002556D;
+	mmdc_p0->mapsr = (u32)0x00011006;
+	mmdc_p0->mdscr = (u32)0x00000000;
+}
+
+//Copy from DCD in flash_header.S
+//CONFIG_MX6DL_DDR3
+static void spl_dram_init_mx6dl_1g(void)
+{
+	volatile struct mmdc_p_regs *mmdc_p0;
+	volatile struct mmdc_p_regs *mmdc_p1;
+	mmdc_p0 = (struct mmdc_p_regs *) MMDC_P0_BASE_ADDR;
+	mmdc_p1 = (struct mmdc_p_regs *) MMDC_P1_BASE_ADDR;
+
+	/* Calibrations */
+	/* ZQ */
+	mmdc_p0->mpzqhwctrl = (u32)0xa1390003;
+
+	/* write leveling */
+	mmdc_p0->mpwldectrl0 = (u32)0x003E0047;
+	mmdc_p0->mpwldectrl1 = (u32)0x00300036;
+	mmdc_p1->mpwldectrl0 = (u32)0x0018001B;
+	mmdc_p1->mpwldectrl1 = (u32)0x0010002C;
+
+	/* DQS gating, read delay, write delay calibration values
+	based on calibration compare of 0x00ffff00 */
+	mmdc_p0->mpdgctrl0 = (u32)0x02380234;
+	mmdc_p0->mpdgctrl1 = (u32)0x02300230;
+	mmdc_p1->mpdgctrl0 = (u32)0x0218021C;
+	mmdc_p1->mpdgctrl1 = (u32)0x0210020C;
+	mmdc_p0->mprddlctl = (u32)0x40464846;
+	mmdc_p1->mprddlctl = (u32)0x40444640;
+	mmdc_p0->mpwrdlctl = (u32)0x3A302E30;
+	mmdc_p1->mpwrdlctl = (u32)0x3630302C;
+	/* read data bit delay */
+	mmdc_p0->mprddqby0dl = (u32)0x33333333;
+	mmdc_p0->mprddqby1dl = (u32)0x33333333;
+	mmdc_p0->mprddqby2dl = (u32)0x33333333;
+	mmdc_p0->mprddqby3dl = (u32)0x33333333;
+	mmdc_p1->mprddqby0dl = (u32)0x33333333;
+	mmdc_p1->mprddqby1dl = (u32)0x33333333;
+	mmdc_p1->mprddqby2dl = (u32)0x33333333;
+	mmdc_p1->mprddqby3dl = (u32)0x33333333;
+	/* Complete calibration by forced measurment */
+	mmdc_p0->mpmur0 = (u32)0x00000800;
+	mmdc_p1->mpmur0 = (u32)0x00000800;
+	/* MMDC init:
+	 in DDR3, 64-bit mode, only MMDC0 is initiated: */
+	mmdc_p0->mdpdc = (u32)0x0002002D;
+	mmdc_p0->mdotc = (u32)0x00333040;
+
+	mmdc_p0->mdcfg0 = (u32)0x3F435313;
+	mmdc_p0->mdcfg1 = (u32)0xB66E8B63;
+
+	mmdc_p0->mdcfg2 = (u32)0x01FF00DB;
+	mmdc_p0->mdmisc = (u32)0x00011740;
+	mmdc_p0->mdscr = (u32)0x00008000;
+	mmdc_p0->mdrwd = (u32)0x000026D2;
+	mmdc_p0->mdor = (u32)0x00431023;
+	mmdc_p0->mdasp = (u32)0x00000027;
+	mmdc_p0->mdctl = (u32)0x831A0000;
+
+	/* MR */
+	mmdc_p0->mdscr = (u32)0x02008032;
+	mmdc_p0->mdscr = (u32)0x00008033;
+	mmdc_p0->mdscr = (u32)0x00048031;
+	mmdc_p0->mdscr = (u32)0x05208030;
+	mmdc_p0->mdscr = (u32)0x04008040;
+
+	/* final DDR setup */
+	mmdc_p0->mdref = (u32)0x00007800;
+	mmdc_p0->mpodtctrl = (u32)0x00022227;
+	mmdc_p1->mpodtctrl = (u32)0x00022227;
+	mmdc_p0->mdpdc = (u32)0x0002556D;
+	mmdc_p0->mapsr = (u32)0x00011006;
+	mmdc_p0->mdscr = (u32)0x00000000;
+}
+
+/* i.MX6Q */
+static void spl_dram_init_mx6q_2g(void)
+{
+	volatile struct mmdc_p_regs *mmdc_p0;
+	volatile struct mmdc_p_regs *mmdc_p1;
+	mmdc_p0 = (struct mmdc_p_regs *) MMDC_P0_BASE_ADDR;
+	mmdc_p1 = (struct mmdc_p_regs *) MMDC_P1_BASE_ADDR;
+
+	/* Calibrations */
+	/* ZQ */
+	mmdc_p0->mpzqhwctrl = (u32)0xa1390003;
+	/* write leveling */
+	mmdc_p0->mpwldectrl0 = (u32)0x00000000;
+	mmdc_p0->mpwldectrl1 = (u32)0x00000000;
+	mmdc_p1->mpwldectrl0 = (u32)0x00000000;
+	mmdc_p1->mpwldectrl1 = (u32)0x00000000;
+	/* DQS gating, read delay, write delay calibration values
+	 based on calibration compare of 0x00ffff00  */
+	mmdc_p0->mpdgctrl0 = (u32)0x032C0340;
+	mmdc_p0->mpdgctrl1 = (u32)0x03300324;
+	mmdc_p1->mpdgctrl0 = (u32)0x032C0338;
+	mmdc_p1->mpdgctrl1 = (u32)0x03300274;
+	
+	mmdc_p0->mprddlctl = (u32)0x423A383E;
+	mmdc_p1->mprddlctl = (u32)0x3638323E;
+
+	mmdc_p0->mpwrdlctl = (u32)0x363C4640;
+	mmdc_p1->mpwrdlctl = (u32)0x4034423C;
+
+	mmdc_p0->mprddqby0dl = (u32)0x33333333;
+	mmdc_p0->mprddqby1dl = (u32)0x33333333;
+	mmdc_p0->mprddqby2dl = (u32)0x33333333;
+	mmdc_p0->mprddqby3dl = (u32)0x33333333;
+	mmdc_p1->mprddqby0dl = (u32)0x33333333;
+	mmdc_p1->mprddqby1dl = (u32)0x33333333;
+	mmdc_p1->mprddqby2dl = (u32)0x33333333;
+	mmdc_p1->mprddqby3dl = (u32)0x33333333;
+
+	mmdc_p0->mpmur0 = (u32)0x00000800;
+	mmdc_p1->mpmur0 = (u32)0x00000800;
+	/* MMDC init:
+	  in DDR3, 64-bit mode, only MMDC0 is initiated: */
+	mmdc_p0->mdpdc = (u32)0x00020036;
+	mmdc_p0->mdotc = (u32)0x09444040;
+	mmdc_p0->mdcfg0 = (u32)0x898E7955;
+	mmdc_p0->mdcfg1 = (u32)0xFF328F64;
+	mmdc_p0->mdcfg2 = (u32)0x01FF00DB;
+	mmdc_p0->mdmisc = (u32)0x00011740;
+
+	mmdc_p0->mdscr = (u32)0x00008000;
+	mmdc_p0->mdrwd = (u32)0x000026d2;
+	mmdc_p0->mdor = (u32)0x008E1023;
+	
+	/* 2G */ 
+	mmdc_p0->mdasp = (u32)0x00000047;
+	mmdc_p0->mdctl = (u32)0x841a0000;
+	
+	/* 1G 
+	mmdc_p0->mdasp = (u32)0x00000027;
+	mmdc_p0->mdctl = (u32)0x831a0000;*/
+
+	mmdc_p0->mdscr = (u32)0x02088032;
+	mmdc_p0->mdscr = (u32)0x00008033;
+	mmdc_p0->mdscr = (u32)0x00048031;
+	mmdc_p0->mdscr = (u32)0x09408030;
+	mmdc_p0->mdscr = (u32)0x04008040;
+
+	mmdc_p0->mdref = (u32)0x00007800;
+	
+	mmdc_p0->mpodtctrl = (u32)0x00022227;
+	mmdc_p1->mpodtctrl = (u32)0x00022227;
+
+	mmdc_p0->mdpdc = (u32)0x00025576;
+	mmdc_p0->mapsr = (u32)0x00011006;
+	mmdc_p0->mdscr = (u32)0x00000000;
+}
+
+static void spl_dram_init(void)
+{	
+	u32 cpurev, imxtype;
+	
+	cpurev = get_cpu_rev();
+	imxtype = (cpurev & 0xFF000) >> 12;
+
+	get_imx_type(imxtype);	
+
+	switch (imxtype){
+	case MXC_CPU_MX6SOLO:
+		spl_mx6dl_dram_setup_iomux();
+		spl_dram_init_mx6solo_512mb();
+		break;
+	case MXC_CPU_MX6Q:
+		spl_mx6q_dram_setup_iomux();
+		spl_dram_init_mx6q_2g();
+		break;
+	case MXC_CPU_MX6DL:
+	default:
+		spl_mx6dl_dram_setup_iomux();
+		spl_dram_init_mx6dl_1g();
+		break;	
+	}
+}
+
+void board_init_f(ulong dummy)
+{	
+	/* Set the stack pointer. */
+	asm volatile("mov sp, %0\n" : : "r"(CONFIG_SPL_STACK));
+
+	spl_dram_init();	
+	
+	arch_cpu_init();
+
+	/* Clear the BSS. */
+	memset(__bss_start, 0, __bss_end - __bss_start);
+
+	/* Set global data pointer. */
+	gd = &gdata;
+
+	board_early_init_f();	
+
+	timer_init();
+	preloader_console_init();
+
+	board_init_r(NULL, 0);
+}
+
+void spl_board_init(void)
+{
+	setup_boot_device();
+}
+
+u32 spl_boot_device(void)
+{
+	puts("Boot Device: ");
+	switch (get_boot_device()) {
+	case MX6_SD0_BOOT:
+		printf("SD0\n");
+		return BOOT_DEVICE_MMC1;
+	case MX6_SD1_BOOT:
+		printf("SD1\n");
+		return BOOT_DEVICE_MMC2;
+	case MX6_MMC_BOOT:
+		printf("MMC\n");
+		return BOOT_DEVICE_MMC2;
+	case MX6_NAND_BOOT:
+		printf("NAND\n");
+		return BOOT_DEVICE_NAND;
+	case MX6_SATA_BOOT:
+		printf("SATA\n");
+		return BOOT_DEVICE_SATA;
+	case MX6_UNKNOWN_BOOT:
+	default:
+		printf("UNKNOWN\n");
+		return BOOT_DEVICE_NONE;
+	}
+}
+
+u32 spl_boot_mode(void)
+{
+	switch (spl_boot_device()) {
+	case BOOT_DEVICE_MMC1:
+	case BOOT_DEVICE_MMC2:
+	case BOOT_DEVICE_MMC2_2:
+		return MMCSD_MODE;
+		break;
+	case BOOT_DEVICE_SATA:
+		return SATA_MODE;
+		break;
+	//case BOOT_DEVICE_NAND:
+	//	return 0;
+	//	break;
+	default:
+		puts("spl: ERROR:  unsupported device\n");
+		hang();
+	}
+}
+
+void reset_cpu(ulong addr)
+{
+	__REG16(WDOG1_BASE_ADDR) = 4;
+}
+#endif
+
---
+++ include/configs/wandboard.h
@@ -0,0 +1,236 @@
+/*
+ * Copyright (C) 2013 Freescale Semiconductor, Inc.
+ *
+ * Configuration settings for the Wandboard.
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#include <asm/arch/imx-regs.h>
+#include <asm/imx-common/gpio.h>
+#include <asm/sizes.h>
+
+#include "imx6_spl.h"
+
+#define CONFIG_MX6
+#define CONFIG_DISPLAY_CPUINFO
+#define CONFIG_DISPLAY_BOARDINFO
+
+#define MACH_TYPE_WANDBOARD		4412
+#define CONFIG_MACH_TYPE		MACH_TYPE_WANDBOARD
+
+#define CONFIG_CMDLINE_TAG
+#define CONFIG_SETUP_MEMORY_TAGS
+#define CONFIG_INITRD_TAG
+#define CONFIG_REVISION_TAG
+
+/* Size of malloc() pool */
+#define CONFIG_SYS_MALLOC_LEN		(10 * SZ_1M)
+
+#define CONFIG_BOARD_EARLY_INIT_F
+#define CONFIG_BOARD_LATE_INIT
+#define CONFIG_MXC_GPIO
+
+#define CONFIG_MXC_UART
+#define CONFIG_MXC_UART_BASE		UART1_BASE
+
+/* allow to overwrite serial and ethaddr */
+#define CONFIG_ENV_OVERWRITE
+#define CONFIG_CONS_INDEX		1
+#define CONFIG_BAUDRATE			115200
+
+/* Command definition */
+#include <config_cmd_default.h>
+
+#undef CONFIG_CMD_IMLS
+
+#define CONFIG_CMD_BMODE
+#define CONFIG_CMD_SETEXPR
+
+#define CONFIG_BOOTDELAY		3
+
+#define CONFIG_SYS_MEMTEST_START	0x10000000
+#define CONFIG_SYS_MEMTEST_END		(CONFIG_SYS_MEMTEST_START + 500 * SZ_1M)
+#define CONFIG_LOADADDR			0x10800000
+#define CONFIG_SYS_TEXT_BASE		0x17800000
+
+#define CONFIG_CMD_SATA
+
+/*
+ * SATA Configs
+ */
+#ifdef CONFIG_CMD_SATA
+#define CONFIG_DWC_AHSATA
+#define CONFIG_SYS_SATA_MAX_DEVICE	1
+#define CONFIG_DWC_AHSATA_PORT_ID	0
+#define CONFIG_DWC_AHSATA_BASE_ADDR	SATA_ARB_BASE_ADDR
+#define CONFIG_LBA48
+#define CONFIG_LIBATA
+#endif
+
+/* MMC Configuration */
+#define CONFIG_FSL_ESDHC
+#define CONFIG_FSL_USDHC
+#define CONFIG_SYS_FSL_USDHC_NUM	2
+#define CONFIG_SYS_FSL_ESDHC_ADDR	0
+
+#define CONFIG_MMC
+#define CONFIG_CMD_MMC
+#define CONFIG_GENERIC_MMC
+#define CONFIG_BOUNCE_BUFFER
+#define CONFIG_CMD_EXT2
+#define CONFIG_CMD_FAT
+#define CONFIG_DOS_PARTITION
+
+/* Ethernet Configuration */
+#define CONFIG_CMD_PING
+#define CONFIG_CMD_DHCP
+#define CONFIG_CMD_MII
+#define CONFIG_CMD_NET
+#define CONFIG_FEC_MXC
+#define CONFIG_MII
+#define IMX_FEC_BASE			ENET_BASE_ADDR
+#define CONFIG_FEC_XCV_TYPE		RGMII
+#define CONFIG_ETHPRIME			"FEC"
+#define CONFIG_FEC_MXC_PHYADDR		1
+#define CONFIG_PHYLIB
+#define CONFIG_PHY_ATHEROS
+
+/* Framebuffer */
+#define CONFIG_VIDEO
+#define CONFIG_VIDEO_IPUV3
+#define CONFIG_CFB_CONSOLE
+#define CONFIG_VGA_AS_SINGLE_DEVICE
+#define CONFIG_SYS_CONSOLE_IS_IN_ENV
+#define CONFIG_SYS_CONSOLE_OVERWRITE_ROUTINE
+#define CONFIG_VIDEO_BMP_RLE8
+#define CONFIG_SPLASH_SCREEN
+#define CONFIG_SPLASH_SCREEN_ALIGN
+#define CONFIG_BMP_16BPP
+#define CONFIG_VIDEO_LOGO
+#define CONFIG_VIDEO_BMP_LOGO
+#define CONFIG_VIDEO_BMP_GZIP
+#define CONFIG_SYS_VIDEO_LOGO_MAX_SIZE (1 << 20)
+#define CONFIG_IPUV3_CLK 260000000
+#define CONFIG_IMX_HDMI
+
+#if defined(CONFIG_MX6DL) || defined(CONFIG_MX6S)
+#define CONFIG_DEFAULT_FDT_FILE		"imx6dl-wandboard.dtb"
+#elif defined(CONFIG_MX6Q)
+#define CONFIG_DEFAULT_FDT_FILE		"imx6q-wandboard.dtb"
+#else
+#define CONFIG_DEFAULT_FDT_FILE		"imx6q-wandboard.dtb"
+#endif
+
+/* USB Configs */
+#define CONFIG_CMD_USB
+#define CONFIG_CMD_FAT
+#define CONFIG_USB_EHCI
+#define CONFIG_USB_EHCI_MX6
+#define CONFIG_USB_STORAGE
+#define CONFIG_MXC_USB_PORT	1
+#define CONFIG_MXC_USB_PORTSC	(PORT_PTS_UTMI | PORT_PTS_PTW)
+#define CONFIG_MXC_USB_FLAGS	0
+
+#define CONFIG_BOOTCOMMAND \
+	"mmc dev ${mmcdev};" \
+	"if mmc rescan; then " \
+	"if run loadbootenv; then " \
+		"echo Loaded environment from ${bootenv};" \
+		"run importbootenv;" \
+	"fi;" \
+	"if test -n $uenvcmd; then " \
+		"echo Running uenvcmd ...;" \
+		"run uenvcmd;" \
+	"fi;" \
+	"if run loadramdisk; then " \
+		"setenv rootdevice ${ramdisk_dev}; " \
+		"setenv bootsys \'bootm ${loadaddr} ${initrdaddr}\'; " \
+	"fi;" \
+	"if run loaduimage; then " \
+		"run setbootargs; " \
+		"run bootsys ; " \
+	"fi;" \
+	"fi;"
+
+#define CONFIG_EXTRA_ENV_SETTINGS \
+	"bootargs_base=console=ttymxc0,115200\0" \
+	"setbootargs=run setbase; run setvideo; run setopts; run setplatform\0" \
+	"setbase=setenv bootargs ${bootargs_base} ${rootdevice}\0" \
+	"setvideo=setenv bootargs ${bootargs} ${video_mode}\0" \
+	"setplatform=if test -n $expansion; then setenv bootargs " \
+		"$bootargs expansion=$expansion;fi;if test -n $baseboard;" \
+		" then setenv bootargs $bootargs baseboard=$baseboard;fi\0" \
+	"setopts=setenv bootargs ${bootargs} ${optargs}\0" \
+	"setvideo=setenv bootargs ${bootargs} ${video_mode}\0" \
+	"bootsys=bootm ${loadaddr}\0 " \
+	"initrdaddr=0x13000000\0" \
+	"rootdevice=root=/dev/mmcblk0p2 rootwait ro rootfstype=ext4\0" \
+	"mmcdev=0\0" \
+	"bootenv=boot/uEnv.txt\0" \
+	"bootcmd="CONFIG_BOOTCOMMAND"\0" \
+	"loadbootenv=fatload mmc ${mmcdev} ${loadaddr} ${bootenv}\0" \
+	"importbootenv=echo Importing environment...; " \
+		"env import -t $loadaddr $filesize\0" \
+	"loaduimage=fatload mmc ${mmcdev} ${loadaddr} boot/uImage\0" \
+	"loaduimage_raw=mmc read ${loadaddr} 0x800 0x4000\0 "	\
+	"loadramdisk=fatload mmc ${mmcdev} ${initrdaddr} boot/uramdisk.img\0" \
+	"splashimage=0x10800000\0"				\
+	"splashimage_mmc_init_block=0x410\0"			\
+	"splashimage_mmc_blkcnt=0x3F0\0"			\
+	"splashimage_file_name=boot/wandboard.bmp.gz\0"		\
+        "splashpos=m,m\0"
+
+/*	"bootcmd=mmc dev " __stringify(CONFIG_SYS_MMC_ENV_DEV) "; mmc read ${loadaddr} 0x800 0x1a00;bootm\0" 
+*/
+
+/* Miscellaneous configurable options */
+#define CONFIG_SYS_LONGHELP
+#define CONFIG_SYS_HUSH_PARSER
+#define CONFIG_SYS_PROMPT	       "WANDBOARD=> "
+#define CONFIG_AUTO_COMPLETE
+#define CONFIG_SYS_CBSIZE		512
+
+/* Print Buffer Size */
+#define CONFIG_SYS_PBSIZE (CONFIG_SYS_CBSIZE + sizeof(CONFIG_SYS_PROMPT) + 16)
+#define CONFIG_SYS_MAXARGS	       32
+#define CONFIG_SYS_BARGSIZE CONFIG_SYS_CBSIZE
+
+#define CONFIG_SYS_LOAD_ADDR		CONFIG_LOADADDR
+#define CONFIG_SYS_HZ			1000
+
+#define CONFIG_CMDLINE_EDITING
+
+/* Physical Memory Map */
+#define CONFIG_NR_DRAM_BANKS		1
+#define PHYS_SDRAM			MMDC0_ARB_BASE_ADDR
+
+#define CONFIG_SYS_SDRAM_BASE		PHYS_SDRAM
+#define CONFIG_SYS_INIT_RAM_ADDR	IRAM_BASE_ADDR
+#define CONFIG_SYS_INIT_RAM_SIZE	IRAM_SIZE
+
+#define CONFIG_SYS_INIT_SP_OFFSET \
+	(CONFIG_SYS_INIT_RAM_SIZE - GENERATED_GBL_DATA_SIZE)
+#define CONFIG_SYS_INIT_SP_ADDR \
+	(CONFIG_SYS_INIT_RAM_ADDR + CONFIG_SYS_INIT_SP_OFFSET)
+
+/* FLASH and environment organization */
+#define CONFIG_SYS_NO_FLASH
+
+#define CONFIG_ENV_SIZE			(8 * 1024)
+
+#define CONFIG_ENV_IS_IN_MMC
+#define CONFIG_ENV_OFFSET		(6 * 64 * 1024)
+#define CONFIG_SYS_MMC_ENV_DEV		0
+
+#define CONFIG_OF_LIBFDT
+#define CONFIG_CMD_BOOTZ
+
+#ifndef CONFIG_SYS_DCACHE_OFF
+#define CONFIG_CMD_CACHE
+#endif
+
+#endif			       /* __CONFIG_H * */
